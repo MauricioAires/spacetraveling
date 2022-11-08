914609f04c9fec430cc18b487ec7e32c772eb459
MauricioAires <mauricioaires015@gmail.com>
2022-11-08 10:31:17 -0300

    feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import S from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={S.Wrapper}>
+      <div>
+        <Link href="/">
+          <a>
+            <Image width="238" height="25" src="/logo.svg" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
